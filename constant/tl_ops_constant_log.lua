668ce4aef109bbab403f7785e3ac458b20502737
tangshimin <www.1905333456@foxmail.com>
2022-02-28 10:40:10 +0800

    fix: path

--- constant/tl_ops_constant_log.lua
+++ constant/tl_ops_constant_log.lua
@@ -1,6 +1,5 @@
 local tl_ops_constant_log = {
-    dir = [[E:\code\tl-open-source\tl-ops-manage\]], ---- log 路径
-    -- dir = [[D:\project\tl-open-source\tl-ops-manage\]],
+    dir = [[/your_project_path/tl-ops-manage/]],
     format_json = true  ---- 是否json格式输出log
 }
 
